eb0a7d8caf35947e63e639b20f94d25ddc5d39d8
Andrew Duthie <andrew@andrewduthie.com>
2016-08-27 16:44:25 -0400

    Incorporate MasterBar page heading component

--- src/layout/app/index.js
+++ src/layout/app/index.js
@@ -3,9 +3,15 @@
  */
 import { h } from 'preact';
 
+/**
+ * Internal dependencies
+ */
+import MasterBar from 'layout/master-bar';
+
 export default function App( { children } ) {
 	return (
 		<main className="app">
+			<MasterBar />
 			{ children }
 		</main>
 	);
---
+++ src/layout/master-bar/index.js
@@ -0,0 +1,14 @@
+/**
+ * External dependencies
+ */
+import { h } from 'preact';
+
+export default function MasterBar() {
+	return (
+		<header className="master-bar">
+			<h1 className="master-bar__brand">
+				WordCamp Schedule
+			</h1>
+		</header>
+	);
+}
