c2284b625fa6024170029c50a4a7dc37d7ed02c7
Andrew Duthie <andrew@andrewduthie.com>
2016-08-27 20:35:34 -0400

    Basic request and display of hardcoded camp sessions

--- src/sections/home/index.js
+++ src/sections/home/index.js
@@ -2,7 +2,33 @@
  * External dependencies
  */
 import { h } from 'preact';
+import { connect } from 'preact-redux';
 
-export default function Home() {
-	return <span>Welcome Home</span>;
+/**
+ * Internal dependencies
+ */
+import QuerySessions from 'components/query-sessions';
+import { getSessions } from 'state/sessions/selectors';
+
+function Home( { sessions = [] } ) {
+	return (
+		<div>
+			<QuerySessions
+				camp="cincinnati"
+				year={ 2016 } />
+			<ul>
+				{ sessions.map( ( session ) => (
+					<li key={ session.ID }>
+						{ session.title }
+					</li>
+				) ) }
+			</ul>
+		</div>
+	);
 }
+
+export default connect( ( state ) => {
+	return {
+		sessions: getSessions( state, 'cincinnati', 2016 )
+	};
+} )( Home );
--- src/state/sessions/selectors.js
+++ src/state/sessions/selectors.js
@@ -6,3 +6,7 @@ import { get } from 'lodash';
 export function isRequestingSessions( state, camp, year ) {
 	return get( state.sessions.requesting, [ camp, year ], false );
 }
+
+export function getSessions( state, camp, year ) {
+	return get( state.sessions.items, [ camp, year ] );
+}
