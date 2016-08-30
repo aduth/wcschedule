383fe88bb47a0a660637d7ce0a7b985843e96b24
Andrew Duthie <aduthie7@gmail.com>
2016-08-29 22:02:15 -0400

    Display camps on home page

--- src/sections/home/index.js
+++ src/sections/home/index.js
@@ -2,7 +2,32 @@
  * External dependencies
  */
 import { h } from 'preact';
+import { connect } from 'preact-redux';
+import { map } from 'lodash';
 
-export default function Home() {
-	return <div>Welcome Home</div>;
+/**
+ * Internal dependencies
+ */
+import QueryCamps from 'components/query-camps';
+import { getCamps } from 'state/camps/selectors';
+
+function Home( { camps } ) {
+	return (
+		<div>
+			<QueryCamps />
+			<ul>
+				{ map( camps, ( camp ) => (
+					<li key={ camp.slug }>
+						{ camp.title }
+					</li>
+				) ) }
+			</ul>
+		</div>
+	);
 }
+
+export default connect( ( state ) => {
+	return {
+		camps: getCamps( state )
+	};
+} )( Home );
--- src/state/camps/selectors.js
+++ src/state/camps/selectors.js
@@ -1,3 +1,7 @@
 export function isRequestingCamps( state ) {
 	return state.camps.requesting;
 }
+
+export function getCamps( state ) {
+	return state.camps.items;
+}
